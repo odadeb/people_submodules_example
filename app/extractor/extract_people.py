7556d2b1667b83afa241c59b84453559f1240a02
odair <odair.junior.rodrigues@gmail.com>
2023-01-12 22:37:57 -0300

    apply submodule

---
+++ app/extractor/extract_people.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from app.extractor.settings import path_files
+
+
+class ExtractorPeople():
+    def __init__(self):
+        self.path_files = way
+        self.subject = 'pessoasJuridicas'
+    
+    def extract():
+        pass
+        
